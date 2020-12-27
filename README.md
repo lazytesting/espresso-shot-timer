9565fb1329a8c569ffe86b17a6042893517d3dfa
Maarten Groeneweg <maarten@groeneweg.eu>
2020-12-27 13:16:07 +0100

    tested version

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Arduino based espresso shot timer with a history function
 ## Features
 - Show target shot time
 - Show current shot time
+- Support multiple beans types (2)
+- Automatic triggered when machine runs
 - Show history of shot timings to support micro adjustments
+- Set target shot time
 
 ## Next steps
-- Automatic triggered when machine runs
-- Set target shot time
 - Plot shots in graph (using oled display)
 - Document schematics
 - Improve readme
-- Support multiple beans
 - Build case
 - Add low water level indicator
--- src/shot_timer/shot_timer.ino
+++ src/shot_timer/shot_timer.ino
@@ -7,55 +7,95 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 int pin = 12; // pin for switch
 int targetTimeUpPin = 11;
 int targetTimeDownPin = 10;
+int beanSelectionPin = 8;
 bool isCounting = false;
 int lastShots[4];
 int targetShotTime = 30;
-int targetTimeUpLastState;
-int targetTimeDownLastState;
+int targetTimeUpLastState = 2;
+int targetTimeDownLastState = 2;
+int beanSelectionLastState = 2;
+int currentBeanSelection = 1;
+int beanSelectionLimit = 2; //number of bean types
+elapsedMillis flakyTimer;
 
 void setup()
 {
   pinMode(pin, INPUT_PULLUP);
   pinMode(targetTimeUpPin, INPUT_PULLUP);
   pinMode(targetTimeDownPin, INPUT_PULLUP);
+  pinMode(beanSelectionPin, INPUT_PULLUP);
+  
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(1,1);
-  lcd.print("Actual : ");
+  printBeanSelection();
 }
 
-void printTargetTime(){
+void updateBeanSelection() {
+  int beanSelectionButton = digitalRead(beanSelectionPin);
+  if (beanSelectionButton == LOW && beanSelectionLastState != beanSelectionButton){    
+    if(currentBeanSelection + 1 <= beanSelectionLimit) {
+      currentBeanSelection++;
+    } else {
+      currentBeanSelection = 1;
+    }
+    
+    clearShotHistory();
+    printBeanSelection(); 
+  }
+  beanSelectionLastState = beanSelectionButton;
+}
+
+void printBeanSelection() {
   lcd.setCursor(1,0);
+  lcd.print("Bean : " + String(currentBeanSelection) + "        ");
+}
+
+void updateTargetTimer() {
+  // Pressing up button
+  int upButton = digitalRead(targetTimeUpPin);
+  if (upButton == LOW && targetTimeUpLastState != upButton){
+    targetShotTime++;
+  }
+  targetTimeUpLastState = upButton;
+
+  // Pressing down button
+  int downButton = digitalRead(targetTimeDownPin);
+  if (downButton == LOW && targetTimeDownLastState != downButton){
+    targetShotTime--;
+  }
+  targetTimeDownLastState = downButton;
+
+  // print
+  lcd.setCursor(1,1);
   lcd.print("Target : " + String(targetShotTime) + "        ");
 }
 
 void startTimer() {
   timer = 0;
   isCounting = true;
-  // clear previous current count
-  lcd.setCursor(10,1);
-  lcd.print("    ");
 }
 
 void stopTimer(){
   isCounting = false;
-  int currentShotTime = timer/1000;
+  int currentShotTime = (timer - flakyTimer) /1000;
   if (currentShotTime < 3) {
     // not counting this, probably a flush
     return;
   }
   
   updateShotHistory(currentShotTime);
-  
-  if( lastShots[1] != 0) {
-    printShothistory();
-  }
+}
+
+void clearShotHistory() {
+  memset(lastShots, 0, sizeof(lastShots));
+  lcd.setCursor(1,3);
+  lcd.print("History:          "); // whipe previous history
 }
 
 void printShothistory() {
-  lcd.setCursor(1,2);
+  lcd.setCursor(1,3);
   lcd.print("History:          "); // whipe previous history
-  lcd.setCursor(10,2); // move to position after colon
+  lcd.setCursor(10,3); // move to position after colon
 
   // print all shots except the last one
   for (int i=1; i<4; i++) {   
@@ -78,43 +118,41 @@ void updateShotHistory(int lastShotTime) {
   lastShots[1] = tempLastShots[0];
   lastShots[2] = tempLastShots[1];
   lastShots[3] = tempLastShots[2];
-}
 
-void printCurrentShotTime() {
-  lcd.setCursor(10,1);
-  lcd.print(timer/1000);
-}
-
-void updateTargetTime() {
-  // Pressing up button
-  int upButton = digitalRead(targetTimeUpPin);
-  if (upButton == LOW && targetTimeUpLastState != upButton){
-    targetShotTime++;
+  if( lastShots[1] != 0) {
+    printShothistory();
   }
-  targetTimeUpLastState = upButton;
-
-  // Pressing down button
-  int downButton = digitalRead(targetTimeDownPin);
-  if (downButton == LOW && targetTimeDownLastState != downButton){
-    targetShotTime--;
-  }
-  targetTimeDownLastState = downButton;
 }
 
-void loop()
-{ 
-  updateTargetTime();
-  printTargetTime();
-  
-  int buttonValue = digitalRead(12);
+void printActualTimer() {
+  if (isCounting) {
+    lcd.setCursor(1,2);
+    String actual = String(timer/1000);
+    lcd.print("Actual : " + actual + "    ");
+  }
+}
+
+void updateActualTimer() {
+  int buttonValue = digitalRead(pin);
   if (buttonValue == LOW){
+    flakyTimer = 0;
     if (!isCounting) {
       startTimer();
     }
-    printCurrentShotTime();
   } else {
-    if (isCounting) {
+    // sensor is bit flaky when pump runs, needs damping
+    if (isCounting && flakyTimer > 1000) {
       stopTimer();
     }
   }
+  printActualTimer();
+}
+
+
+
+void loop()
+{ 
+  updateBeanSelection();
+  updateTargetTimer();
+  updateActualTimer();
 }
